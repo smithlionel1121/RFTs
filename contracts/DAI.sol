e22ab9d04f68eac16d02ef219c6c1e12cead2835
Smithlionel1121 <smithlionel1121@gmail.com>
2020-11-28 20:47:25 +0000

    Initial Commit

---
+++ contracts/DAI.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.7.5;
+
+import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
+
+contract DAI is ERC20 {
+    constructor() ERC20('DAI Stablecoin', 'DAI') {}
+
+    function mint(address to, uint amount) external {
+        _mint(to, amount);
+    }
+}
---
+++ contracts/NFT.sol
@@ -0,0 +1,20 @@
+pragma solidity ^0.7.5;
+
+import '@openzeppelin/contracts/token/ERC721/ERC721.sol';
+
+
+contract NFT is ERC721 {
+    constructor(
+        string memory name,
+        string memory symbol
+        )
+
+    ERC721(name, symbol) {
+
+    }
+
+    function mint(address to, uint tokenId) external {
+        _mint(to, tokenId);
+    }
+
+}
